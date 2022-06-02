5787e522205042cf7886ef65e75e6ec300052804
Rupam Debnath <44625252+rupamdebnath@users.noreply.github.com>
2022-06-02 13:16:14 +0530

    Fixed achievement stats for bullets - 10, 25 and 50 bullets on fired, added coroutine ToggleAction to show the Toggle for respective Achievements

--- src/Assets/Scripts/Services/UIService.cs
+++ src/Assets/Scripts/Services/UIService.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIService : MonoBehaviour
 {
     public TextMeshProUGUI BulletCountText;
     public TextMeshProUGUI EnemiesKilledText;
-
+    public Toggle achievementToggle;
     private void OnEnable()
     {
         ServiceEvents.Instance.OnFire += GetBulletsFired;
@@ -25,13 +26,13 @@ public class UIService : MonoBehaviour
         switch(value)
         {
             case 10:
-                Debug.Log("Bullet Achievement 1 Completed, 10 bullets successfully fired");
+                StartCoroutine(ToggleAction("Achievement Unlocked : Fire 10 bullets"));
                 break;
             case 25:
-                Debug.Log("Bullet Achievement 2 Completed, 25 bullets successfully fired");
+                StartCoroutine(ToggleAction("Achievement Unlocked : Fire 25 bullets"));
                 break;
             case 50:
-                Debug.Log("Bullet Achievement 3 Completed, 50 bullets successfully fired");
+                StartCoroutine(ToggleAction("Achievement Unlocked : Fire 50 bullets"));
                 break;
         }
     }
@@ -39,5 +40,18 @@ public class UIService : MonoBehaviour
     void ShowEnemiesKilled(int value)
     {
         EnemiesKilledText.text = "Enemies Killed: " + value;
+    }
+
+    IEnumerator ToggleAction(string achievementText)
+    {
+        achievementToggle.gameObject.SetActive(true);
+        achievementToggle.gameObject.GetComponentInChildren<TextMeshProUGUI>().text = achievementText;
+        yield return new WaitForSeconds(1);
+        achievementToggle.isOn = true;
+        achievementToggle.gameObject.GetComponentInChildren<TextMeshProUGUI>().fontStyle = FontStyles.Strikethrough;
+        yield return new WaitForSeconds(2);
+        achievementToggle.gameObject.SetActive(false);
+        achievementToggle.isOn = false;
+        achievementToggle.gameObject.GetComponentInChildren<TextMeshProUGUI>().fontStyle = FontStyles.Bold;
     }
 }
