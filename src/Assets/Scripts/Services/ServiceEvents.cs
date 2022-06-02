e2ecc3e49ffc998794d651336e917734a0480f64
Rupam Debnath <44625252+rupamdebnath@users.noreply.github.com>
2022-06-02 12:03:00 +0530

    BUllets fired achievement added, Debug.Log respectively tested and working as exxpected

--- src/Assets/Scripts/Services/ServiceEvents.cs
+++ src/Assets/Scripts/Services/ServiceEvents.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 
 public class ServiceEvents : SingletonGeneric <ServiceEvents>
 {
-
     public Action<int> ScoreCounter;
 
 	public Action<int> OnEnemyDeath;
--- src/Assets/Scripts/Services/UIService.cs
+++ src/Assets/Scripts/Services/UIService.cs
@@ -22,6 +22,18 @@ public class UIService : MonoBehaviour
     void GetBulletsFired(int value)
     {
         BulletCountText.text = "Bullets Fired: " + value;
+        switch(value)
+        {
+            case 10:
+                Debug.Log("Bullet Achievement 1 Completed, 10 bullets successfully fired");
+                break;
+            case 25:
+                Debug.Log("Bullet Achievement 2 Completed, 25 bullets successfully fired");
+                break;
+            case 50:
+                Debug.Log("Bullet Achievement 3 Completed, 50 bullets successfully fired");
+                break;
+        }
     }
 
     void ShowEnemiesKilled(int value)
