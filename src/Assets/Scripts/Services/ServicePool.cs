eba10d7267a062a3adbfb83fd626b29c3e0d6eb7
Rupam Debnath <44625252+rupamdebnath@users.noreply.github.com>
2022-06-03 20:45:50 +0530

    Added ServicePool.cs generic Object Pool class

---
+++ src/Assets/Scripts/Services/ServicePool.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ServicePool<T> : SingletonGeneric<ServicePool<T>> where T : class
+{
+    private List<PooledItem<T>> pooledItems;
+
+    public virtual T GetItem()
+    {
+        if (pooledItems.Count > 0)
+        {
+            PooledItem<T> item = pooledItems.Find(i => i.IsUsed == false);
+            if (item != null)
+            {
+                item.IsUsed = true;
+                return item.Item;
+            }
+            return CreateNewPooledItem();
+        }
+        return CreateNewPooledItem();
+    }
+
+    private T CreateNewPooledItem()
+    {
+        PooledItem<T> pooledItem = new PooledItem<T>();
+        pooledItem.Item = CreateItem();
+        pooledItem.IsUsed = true;
+        pooledItems.Add(pooledItem);
+        return pooledItem.Item;
+    }
+
+    public virtual void ReturnItem(T item)
+    {
+        PooledItem<T> pooledItem = pooledItems.Find(i => i.Item.Equals(item));
+        pooledItem.IsUsed = false;
+    }
+
+    protected virtual T CreateItem()
+    {
+        return (T) null;
+    }
+    private class PooledItem<t>
+    {
+        public t Item;
+        public bool IsUsed;
+    }
+}
+
